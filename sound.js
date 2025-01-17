346c189f46e42f676968f3be8365e395a77a4a71
juliicory <julicorydora@gmail.com>
2025-01-17 12:14:15 -0500

    yes

---
+++ sound.js
@@ -0,0 +1,21 @@
+/*function choose()
+{
+    const audio = [
+
+    ];
+    let last = audio.length;
+    let chosen = Math.floor(Math.random() * (last+1));
+}
+
+function play()
+{
+    const sound = document.getElementById("myBtn").onclick = displayDate;
+    image.src = chosen;
+}
+*/const audio = [
+    https://www.computerhope.com/jargon/m/example.mp3
+];
+let last = audio.length;
+let chosen = Math.floor(Math.random() * (last+1));
+const sound = document.getElementById("sound").onclick = displayDate;
+image.src = chosen;
