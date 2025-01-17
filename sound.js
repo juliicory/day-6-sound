0d074ece1a3a0868a6cda4766a3e3f61c17a9227
juliicory <julicorydora@gmail.com>
2025-01-17 12:40:41 -0500

    new

--- sound.js
+++ sound.js
@@ -12,10 +12,10 @@ function play()
     const sound = document.getElementById("myBtn").onclick = displayDate;
     image.src = chosen;
 }
-*/const audio = [
+const audio = [
     https://www.computerhope.com/jargon/m/example.mp3
 ];
 let last = audio.length;
 let chosen = Math.floor(Math.random() * (last+1));
 const sound = document.getElementById("sound").onclick = displayDate;
-image.src = chosen;
+image.src = chosen;*/
